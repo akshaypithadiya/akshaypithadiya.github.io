e128bae7f1cc9954fe14afbc327c2fc7989e8eb8
Akshay Pithadiya <akshay.pithadiya@gmail.com>
2021-02-28 17:40:15 +0530

    project component done

---
+++ src/components/project/Project.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Project.scss";
+
+const Project = (props) => {
+  const { name, year, desc, website_url, github_url } = props;
+
+  return (
+    <div className="Project">
+      <p className="project-header">
+        <span className="project-name">{name}</span>
+        <span className="project-year">
+          <i>{year}</i>
+        </span>
+      </p>
+      <p className="project-body">{desc}</p>
+      <p className="project-footer">
+        <span>
+          {website_url && (
+            <a href={website_url} target="_blank">
+              WEBSITE
+            </a>
+          )}
+          {github_url && (
+            <a href={github_url} target="_blank">
+              GITHUB
+            </a>
+          )}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Project;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,54 @@
 import React from "react";
+import Project from "../components/project/Project";
 
 const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      name: "confviz",
+      year: "2021",
+      desc:
+        "Statistics, Visualizations and Paper Links of Machine Learning Conferences",
+      website_url: "https://confviz.now.sh/",
+      github_url: "https://github.com/akshaypithadiya/confviz",
+    },
+    {
+      id: 2,
+      name: "TorchExpo",
+      year: "2020",
+      desc:
+        "Collection of models and extensions for mobile deployment in PyTorch",
+      website_url: "https://torchexpo.xyz/",
+      github_url: null,
+    },
+    {
+      id: 3,
+      name: "connex",
+      year: "2018",
+      desc:
+        "Portal for college where students can write posts, take notes, upload photos, add comments and connect with other students",
+      website_url: null,
+      github_url: "https://github.com/akshaypithadiya/connex",
+    },
+  ];
   return (
     <div className="Projects">
       <center>
         <h2>Projects</h2>
-        <h3>Work in progress...</h3>
       </center>
+      {projects.map((project) => {
+        const { id, name, year, desc, website_url, github_url } = project;
+        return (
+          <Project
+            key={id}
+            name={name}
+            year={year}
+            desc={desc}
+            website_url={website_url}
+            github_url={github_url}
+          />
+        );
+      })}
     </div>
   );
 };
